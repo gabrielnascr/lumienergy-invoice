2a50e0efcfa5f96fa2d558ac19f7b0320b6298a2
Gabriel <gr153tt@gmail.com>
2024-05-25 11:27:02 -0300

    feat: adding firebase storage service

---
+++ server/src/core/storage/FirebaseStorageService.ts
@@ -0,0 +1,39 @@
+import {
+  ref,
+  uploadBytes,
+  deleteObject,
+  getDownloadURL,
+} from "firebase/storage";
+
+import { storage } from "../../config/firebase";
+
+export class FirebaseStorageService {
+  async uploadFile(
+    path: string,
+    file: Buffer,
+    filename: string
+  ): Promise<string> {
+    const fileRef = ref(storage, `${path}/${filename}`);
+    const metadata = {
+      contentType: "application/pdf",
+    };
+    try {
+      const snapshot = await uploadBytes(fileRef, file, metadata);
+      const downloadURL = await getDownloadURL(snapshot.ref);
+      return downloadURL;
+    } catch (error) {
+      console.error("Error uploading file:", error);
+      throw error;
+    }
+  }
+
+  async deleteFile(path: string, filename: string): Promise<void> {
+    const fileRef = ref(storage, `${path}/${filename}`);
+
+    try {
+      await deleteObject(fileRef);
+    } catch (error) {
+      throw error;
+    }
+  }
+}
